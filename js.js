93ad2e721c87643c7fb01b144b01195881e07d5b
coderdecember29 <96750361+coderdecember29@users.noreply.github.com>
2023-01-19 16:33:10 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,47 @@
+var song = "";
+var song2 = "";
+
+
+leftwristx = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy = 0;
+function preload(){
+    song = loadSound("music.mp3");
+
+}
+
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is initialized')
+}
+function draw(){
+image(video,0,0,600,500);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristy = results[0].pose.leftWrist.y;
+    rightwristx = results[0].pose.rightWrist.x;
+    rightwristy = results[0].pose.rightWrist.y;
+        console.log("leftwristx = "+ leftwristx +"leftwristy = " + leftwristy);
+        console.log("rightwristx = "+ rightwristx + "rightleftwristy = " + rightwristy) ; 
+    }
+}
